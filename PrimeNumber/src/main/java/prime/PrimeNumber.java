0022e8007be362e281ed16a003db14c11d4b9ecd
viharika <v.viharika999@gmail.com>
2019-06-07 15:56:32 +0530

    commit

---
+++ PrimeNumber/src/main/java/prime/PrimeNumber.java
@@ -0,0 +1,40 @@
+package prime;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int n = sc.nextInt();
+		
+		prime(n);
+	}
+		public static int prime(int n)
+		{
+			String primenumber = " ";
+		for(int i = 1; i < n; i++) {
+			int count = 0;
+			for (int num = i; num >= 1; num--) {
+				if (i % num == 0) {
+					count = count + 1;
+				}
+
+			}
+			if (count == 2) {
+				primenumber = primenumber + i + " ";
+
+			}
+
+		
+			
+		}
+
+		System.out.println(primenumber);
+		return n;
+
+		
+		}
+}
---
+++ PrimeNumber/src/test/java/pkg/TestPrime.java
@@ -0,0 +1,27 @@
+package pkg;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import prime.PrimeNumber;
+public class TestPrime {
+
+	@Test
+	public void test() {
+	int x=PrimeNumber.prime(5);
+	Integer s=Integer.valueOf(x);
+	
+		
+	}
+
+	private void assertEquals(int i, int j, int k, int x) {
+		
+		 i=2;
+		 j=3;
+		 k=5;
+		 
+	}
+
+	
+}
